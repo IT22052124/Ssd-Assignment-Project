9e7e3959d344d4a36ef191f05aaa034106be78a9
Faizal <faizalamaan2@gmail.com>
2025-09-24 19:33:58 +0530

    Revert "ok"
    This reverts commit 6c8a4adc4a58103e970d05046e3bc274bc92eb64.

--- frontend/src/Shared/Components/UiElements/CustomerHeader.jsx
+++ frontend/src/Shared/Components/UiElements/CustomerHeader.jsx
@@ -148,13 +148,14 @@ const CustomerHeader = (props) => {
                     src={
                       customer?.image?.startsWith("http")
                         ? customer.image
-                        : `https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_incoming&w=740&q=80`
+                        : `http://localhost:5000/${customer?.image}`
                     }
+                    alt="User"
                     className="h-10 w-10 rounded-full object-cover shadow"
                     onError={(e) => {
                       e.currentTarget.onerror = null;
                       e.currentTarget.src =
-                        "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_incoming&w=740&q=80";
+                        "https://flowbite.com/docs/images/people/profile-picture-5.jpg";
                     }}
                   />
                 </span>
